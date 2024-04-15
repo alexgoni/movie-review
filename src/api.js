39c31f30f4e00ac056f8dc205cf7650fb2534c0d
alexgoni <goni000211@gmail.com>
2024-04-15 11:49:55 +0900

    feat: pagination

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export async function getReviews({
+  order = "createdAt",
+  offset = 0,
+  limit = 6,
+}) {
+  const query = `order=${order}&offset=${offset}&limit=${limit}`;
+  const response = await fetch(
+    `https://learn.codeit.kr/9504/film-reviews?${query}`
+  );
+  const body = await response.json();
+
+  return body;
+}
--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,56 @@
 import React, { useEffect, useState } from "react";
 import ReviewList from "./ReviewList";
-import mockItems from "../mock.json";
+import { getReviews } from "../api";
+
+const LIMIT = 6;
 
 export default function App() {
   const [order, setOrder] = useState("createdAt");
-  const [items, setItems] = useState(mockItems);
-
-  useEffect(() => {
-    setItems((prev) => [...prev.sort((a, b) => b[order] - a[order])]);
-  }, [order]);
+  const [items, setItems] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [hasNext, setHasNext] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadingError, setLoadingError] = useState(null);
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
 
   const handleDelete = (id) => {
     const nextItems = items.filter((item) => item.id !== id);
     setItems(nextItems);
   };
+
+  const handleLoad = async (options) => {
+    let result;
+
+    try {
+      setIsLoading(true);
+      setLoadingError(null);
+      result = await getReviews(options);
+    } catch (error) {
+      console.error(error);
+      setLoadingError(error);
+    } finally {
+      setIsLoading(false);
+    }
+
+    const { reviews, paging } = result;
+
+    if (options.offset === 0) {
+      setItems(reviews);
+    } else {
+      setItems([...items, ...reviews]);
+    }
+
+    setOffset(options.offset + reviews.length);
+    setHasNext(paging.hasNext);
+  };
+
+  const handleLoadMore = () => {
+    handleLoad({ order, offset, limit: LIMIT });
+  };
+
+  useEffect(() => {
+    handleLoad({ order, offset: 0, limit: LIMIT });
+  }, [order]);
 
   return (
     <div>
@@ -21,7 +58,13 @@ export default function App() {
         <button onClick={() => setOrder("createdAt")}>최신순</button>
         <button onClick={() => setOrder("rating")}>평점순</button>
       </div>
-      <ReviewList items={items} onDelete={handleDelete} />
+      <ReviewList items={sortedItems} onDelete={handleDelete} />
+      {hasNext && (
+        <button disabled={isLoading} onClick={handleLoadMore}>
+          더 보기
+        </button>
+      )}
+      {loadingError?.messages && <span>{loadingError?.messages}</span>}
     </div>
   );
 }
